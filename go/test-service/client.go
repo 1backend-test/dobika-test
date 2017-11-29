5204cee437517344407f537de3cba2054a65f14c
Janos Dobronszki <dobronszki@gmail.com>
2017-11-29 17:45:48 +0100

    Commit for build noIwK04yy

---
+++ go/test-service/client.go
@@ -0,0 +1,29 @@
+package test-service
+
+import(
+	"github.com/1backend/go-client"
+)
+
+var Token string
+
+type Test struct {
+	Name	string
+	Foods	[]string
+}
+
+
+func GetHi(howManyTimes int64, allCool bool) error {
+	var ret 
+	return ret, goclient.New(Token).Call("dobika-test", "test-service", "GET", "/hi", map[string]interface{}{ "howManyTimes": howManyTimes, "allCool": allCool }, &ret)
+}
+
+func GetImportedHi() error {
+	var ret 
+	return ret, goclient.New(Token).Call("dobika-test", "test-service", "GET", "/imported-hi", map[string]interface{}{  }, &ret)
+}
+
+func GetSqlExample() error {
+	var ret 
+	return ret, goclient.New(Token).Call("dobika-test", "test-service", "GET", "/sql-example", map[string]interface{}{  }, &ret)
+}
+
---
+++ ng/test-service/src/test-service.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { NgClient } from '@1backend/ng-client';
+
+
+
+export interface Test {
+	name:	string;
+	foods:	string[];
+}
+
+
+@Injectable()
+export class Service {
+  constructor(private ngClient: NgClient) {}
+
+  GetHi(howManyTimes: number, allCool: boolean): Promise<void> {
+    return this.ngClient.call<void>("dobika-test", "test-service", "GET", "/hi", { "howManyTimes": howManyTimes, "allCool": allCool });
+  }
+
+  GetImportedHi(): Promise<void> {
+    return this.ngClient.call<void>("dobika-test", "test-service", "GET", "/imported-hi", {  });
+  }
+
+  GetSqlExample(): Promise<void> {
+    return this.ngClient.call<void>("dobika-test", "test-service", "GET", "/sql-example", {  });
+  }
+
+}
